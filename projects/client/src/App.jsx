3169e205003b00225da7ecec7f031d729be1fb12
Muhammad Fikri Fernanda Yusuf <fernanda.fikri11@gmail.com>
2022-11-22 18:53:14 +0700

    G3-28
    sudah fix bisa login admin ke admin dashboard dan login user ke home page

--- projects/client/src/App.jsx
+++ projects/client/src/App.jsx
@@ -7,66 +7,74 @@ import { useDispatch } from "react-redux"
 import { axiosInstance } from "./api"
 import { login } from "./redux/features/authSlice"
 import GuestRoute from "./component/GuestRoute"
+import AdminRoute from "./component/admin/AdminRoute"
+import AdminDashboard from "./components/admin/AdminDashboard"
 
 function App() {
-  const [message, setMessage] = useState("")
+    const [message, setMessage] = useState("")
 
-  useEffect(() => {
-    (async () => {
-      const { data } = await axios.get(
-        `${process.env.REACT_APP_API_BASE_URL}/greetings`
-      )
-      setMessage(data?.message || "")
-    })()
-  }, [])
+    useEffect(() => {
+        ;(async () => {
+            const { data } = await axios.get(
+                `${process.env.REACT_APP_API_BASE_URL}/greetings`
+            )
+            setMessage(data?.message || "")
+        })()
+    }, [])
 
-  const [authCheck, setAuthCheck] = useState(false)
+    const [authCheck, setAuthCheck] = useState(false)
 
-  const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-  const keepUserLoggedIn = async () => {
-    try {
-      const auth_token = localStorage.getItem("auth_token")
+    const keepUserLoggedIn = async () => {
+        try {
+            const auth_token = localStorage.getItem("auth_token")
 
-      if (!auth_token) {
-        setAuthCheck(true)
-        return
-      }
+            if (!auth_token) {
+                setAuthCheck(true)
+                return
+            }
 
-      const response = await axiosInstance.get("/auth/refresh-token")
+            const response = await axiosInstance.get("/auth/refresh-token")
 
-      dispatch(login(response.data.data))
+            dispatch(login(response.data.data))
 
-      localStorage.setItem("auth_token", response.data.token)
-      setAuthCheck(true)
-
-    } catch (err) {
-      console.log(err)
-      setAuthCheck(true)
-
-    } finally {
-      setAuthCheck(true)
+            localStorage.setItem("auth_token", response.data.token)
+            setAuthCheck(true)
+        } catch (err) {
+            console.log(err)
+            setAuthCheck(true)
+        } finally {
+            setAuthCheck(true)
+        }
     }
-  }
 
-  useEffect(() => {
-    keepUserLoggedIn()
-  }, [])
+    useEffect(() => {
+        keepUserLoggedIn()
+    }, [])
 
-  return (
-    <>
-      <Routes>
-        <Route
-          path="/login"
-          element={
-            <GuestRoute>
-              <LoginPage />
-            </GuestRoute>
-          }
-        />
-      </Routes>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route
+                    path="/login"
+                    element={
+                        <GuestRoute>
+                            <LoginPage />
+                        </GuestRoute>
+                    }
+                />
+                <Route
+                    path="/admin-dashboard"
+                    element={
+                        <AdminRoute>
+                            <AdminDashboard />
+                        </AdminRoute>
+                    }
+                />
+            </Routes>
+        </>
+    )
 }
 
 export default App
