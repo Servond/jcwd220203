46d7c2941a435843d496257276e1de2573036c4b
Muhammad Fikri Fernanda Yusuf <fernanda.fikri11@gmail.com>
2022-11-30 15:29:51 +0700

    ProductController sementara

--- projects/server/controllers/productController.js
+++ projects/server/controllers/productController.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize")
 const db = require("../models")
 const Product = db.Product
 const Image = db.Image
@@ -6,17 +7,82 @@ const Category = db.Category
 const productController = {
     getAllProduct: async (req, res) => {
         try {
-            const findAllProduct = await Product.findAll({
-                // include: [
-                //     { model: db.Category },
-                //     { model: db.Total_Stock },
-                //     { model: db.Image },
-                // ],
-            })
+            const {
+                product_name = "",
+                description = "",
+                price = "",
+                CategoryId = "",
+                _sortBy = "id",
+                _sortDir = "ASC",
+                _limit = 12,
+                _page = 1,
+            } = req.query
 
+            if (
+                _sortBy === "product_name" ||
+                _sortBy === "description" ||
+                _sortBy === "price" ||
+                _sortBy === "CategoryId" ||
+                product_name ||
+                price ||
+                description ||
+                CategoryId
+            ) {
+                if (!Number(CategoryId)) {
+                    const getAllProducts = await Product.findAndCountAll({
+                        limit: Number(_limit),
+                        offset: (_page - 1) * _limit,
+                        include: [{ model: Category, required: true }],
+                        order: [[_sortBy, _sortDir]],
+                        where: {
+                            [Op.or]: {
+                                product_name: {
+                                    [Op.like]: `%${product_name}%`,
+                                },
+                            },
+                        },
+                    })
+                    return res.status(200).json({
+                        message: "Get all products",
+                        data: getAllProducts.rows,
+                        dataCount: getAllProducts.count,
+                    })
+                }
+
+                const getAllProduct = await Product.findAndCountAll({
+                    limit: Number(_limit),
+                    offset: (_page - 1) * _limit,
+                    include: [{ model: Category, required: true }],
+                    order: [[_sortBy, _sortDir]],
+                    where: {
+                        [Op.or]: {
+                            product_name: {
+                                [Op.like]: `%${product_name}%`,
+                            },
+                        },
+                        CategoryId,
+                    },
+                })
+
+                return res.status(200).json({
+                    message: "Get all books",
+                    data: getAllProduct.rows,
+                    dataCount: getAllProduct.count,
+                })
+            }
+            const getAll = await Product.findAndCountAll({
+                limit: Number(_limit),
+                offset: (_page - 1) * _limit,
+                include: [{ model: Category, required: true }],
+                order: [
+                    [_sortBy, _sortDir],
+                    ["price", "DESC"],
+                ],
+            })
             return res.status(200).json({
-                message: "Get all product",
-                data: findAllProduct,
+                message: "Get all",
+                data: getAll.rows,
+                dataCount: getAll.count,
             })
         } catch (err) {
             console.log(err)
@@ -67,7 +133,15 @@ const productController = {
 
     getCategory: async (req, res) => {
         try {
-            const findCategory = await Category.findAll()
+            const { _limit = 10, _page = 1, _sortBy = "id" } = req.query
+            const findCategory = await Category.findAll({
+                limit: Number(_limit),
+                offset: (_page - 1) * _limit,
+                order: [
+                    ["category_name", "ASC"],
+                    [_sortBy, "ASC"],
+                ],
+            })
             return res.status(200).json({
                 message: "Get category",
                 data: findCategory,
