fdec22d375c680a866f8bacc9c11289ead9544aa
joel374 <88383646+joel374@users.noreply.github.com>
2022-12-22 13:50:11 +0700

    G3-38

--- projects/server/controllers/adminOrderController.js
+++ projects/server/controllers/adminOrderController.js
@@ -553,14 +553,13 @@ const adminOrderController = {
               { model: db.Warehouse },
             ],
           })
-          
+
           return res.status(200).json({
             message: "Waiting Confrimation And atas bet",
             data: response.rows,
             dataCount: response.count,
           })
         }
-
 
         if (payment_method && Number(OrderStatusId)) {
           const response = await db.Transaction.findAndCountAll({
@@ -590,7 +589,7 @@ const adminOrderController = {
               { model: db.Warehouse },
             ],
           })
-          
+
           return res.status(200).json({
             message: "Waiting Confrimation And",
             data: response.rows,
@@ -1214,31 +1213,31 @@ const adminOrderController = {
         include: [{ model: db.User }, { model: db.Warehouse }],
       })
 
-      // const totalBill = findApproveTrasanction.total_price
-      // const paymentDate = moment(findApproveTrasanction.payment_date).format(
-      //   "dddd, DD MMMM YYYY, HH:mm:ss"
-      // )
-      // const transactionLink = `${process.env.BASE_URL_FE}transaction-list`
+      const totalBill = findApproveTrasanction.total_price
+      const paymentDate = moment(findApproveTrasanction.payment_date).format(
+        "dddd, DD MMMM YYYY, HH:mm:ss"
+      )
+      const transactionLink = `${process.env.BASE_URL_FE}transaction-list`
 
-      // const rawHTML = fs.readFileSync("templates/approvePayment.html", "utf-8")
+      const rawHTML = fs.readFileSync("templates/approvePayment.html", "utf-8")
 
-      // const compiledHTML = handlebars.compile(rawHTML)
+      const compiledHTML = handlebars.compile(rawHTML)
 
-      // const htmlResult = compiledHTML({
-      //   username: findApproveTrasanction.User.username,
-      //   totalBill: totalBill.toLocaleString(),
-      //   paymentMethod: findApproveTrasanction.payment_method,
-      //   dateAndTime: `${paymentDate} WIB`,
-      //   transactionListLink: transactionLink,
-      //   shopediaLink: process.env.BASE_URL_FE,
-      // })
+      const htmlResult = compiledHTML({
+        username: findApproveTrasanction.User.username,
+        totalBill: totalBill.toLocaleString(),
+        paymentMethod: findApproveTrasanction.payment_method,
+        dateAndTime: `${paymentDate} WIB`,
+        transactionListLink: transactionLink,
+        shopediaLink: process.env.BASE_URL_FE,
+      })
 
-      // await emailer({
-      //   to: findApproveTrasanction.User.email,
-      //   html: htmlResult,
-      //   subject: "Payment Verified",
-      //   text: "Thank You",
-      // })
+      await emailer({
+        to: findApproveTrasanction.User.email,
+        html: htmlResult,
+        subject: "Payment Verified",
+        text: "Thank You",
+      })
 
       return res.status(200).json({
         message: "Payment Approved",
@@ -1312,6 +1311,47 @@ const adminOrderController = {
       return res.status(200).json({
         message: "Payment Rejected",
         data: findTransaction,
+      })
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({
+        message: "Server Error",
+      })
+    }
+  },
+  sendOrder: async (req, res) => {
+    try {
+      const { id } = req.params
+
+      await db.Transaction.update(
+        {
+          OrderStatusId: 3,
+        },
+        {
+          where: {
+            id: id,
+          },
+        }
+      )
+
+      const dueDateConfirm = moment()
+        .add(5, "minutes")
+        .format("YYYY-MM-DD HH:mm:ss")
+
+      console.log(dueDateConfirm)
+      if (new Date() === dueDateConfirm) {
+        await db.Transaction.update(
+          {
+            OrderStatusId: 4,
+          },
+          {
+            id: id,
+          }
+        )
+      }
+
+      return res.status(200).json({
+        message: "Order Send",
       })
     } catch (error) {
       console.log(error)
